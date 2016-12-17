cd5e75226d53b2df9159899ea20e1ccb02554ac3
Mike Carnes <mcarnes@gmail.com>
2016-12-17 12:06:37 -0600

    Generate a random complex password of a desired length
    I found a module awhile back that used three separate functions to
    generate a set-length complex password.  I built upon that to generate a
    random, complex password of a length of your choice using upper and
    lower characters, as well as numbers and special characters.

---
+++ Get-ComplexPassword.ps1
@@ -0,0 +1,73 @@
+﻿function Get-ComplexPassword {
+    param(
+    [int]$length = 16
+
+    )
+
+    $charlower = 'abcdefghikmnprstuvwxyz'
+    $charupper = 'ABCDEFGHKLMNPRSTUVWXYZ'
+    $nums = '23456789'
+    $symbs = '$%&/()=?*+#'
+
+    #Generate Random lowercase
+	$random = 1..([math]::Round($length/4)) | foreach { Get-Random -Maximum $charlower.Length}
+    $private:ofs=""
+    $password = [string]$charlower[$random]
+
+    #Generate Random Uppercase
+	$random = 1..([math]::Round($length/4)) | foreach { Get-Random -Maximum $charupper.Length}
+    $private:ofs=""
+    $password += [string]$charupper[$random]
+
+    #Generate Random Uppercase
+	$random = 1..([math]::Round($length/4)) | foreach { Get-Random -Maximum $nums.Length}
+    $private:ofs=""
+    $password += [string]$nums[$random]
+
+    #Generate Random Uppercase
+	$random = 1..([math]::Round($length/4)) | foreach { Get-Random -Maximum $symbs.Length}
+    $private:ofs=""
+    $password += [string]$symbs[$random]
+
+    #Generate One additional random character
+    $full = $charlower + $charupper + $nums + $symbs
+    $random = 1..1 | foreach { Get-Random -Maximum $full.Length}
+    $private:ofs=""
+    $password += [string]$full[$random]
+	
+    if ($password.Length -lt ($length + 1)){
+        
+        Do{
+        #Generate One additional random character
+        $full = $charlower + $charupper + $nums + $symbs
+        $random = 1..1 | foreach { Get-Random -Maximum $full.Length}
+        $private:ofs=""
+        $password += [string]$full[$random]
+        }
+
+        Until ($password.Length -eq ($length + 1))
+    }
+    elseif ($password.Length -gt ($length + 1)){
+        Do{
+        $number = $password.length -1
+	    $random = Get-Random -InputObject (0..$number) -Count $number       
+        $private:ofs=''
+	    $password = [String]$password[$random]
+        }
+        Until ($password.Length -eq ($length + 1))
+
+    }	
+
+
+    #Randomize the contents of the generated password
+	$number = $password.length -1
+	$randomize = Get-Random -InputObject (0..$number) -Count $number
+    
+   	$private:ofs=''
+	[String]$password[$randomize]
+
+
+    
+
+
+}
